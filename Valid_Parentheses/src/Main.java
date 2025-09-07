37cad976009ba6d3ffd77d19b22adbbf98fff514
Muhammet Öztürk <180087422+kaiser-dot-exe@users.noreply.github.com>
2025-09-07 11:30:31 +0300

    refactor

---
+++ Valid_Parentheses/src/Main.java
@@ -0,0 +1,30 @@
+import  java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (map.containsValue(c)) {
+                stack.push(c);
+            } else if (map.containsKey(c)) {
+                if (stack.isEmpty() || map.get(c) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+
+
+
+}
